4dab08756b0e79ea423fde589547339bce028c7d
Nithes Kumar S <141005233+Nitheskumar123@users.noreply.github.com>
2025-08-06 21:04:00 +0530

    urls.py

--- resfinal/urls.py
+++ resfinal/urls.py
@@ -13,22 +13,6 @@ urlpatterns = [
     path("profile/update", views.update_profile, name="update-profile"),
     path("profile", views.profile, name="profile"),
     path("about", views.about, name="about"),
-    path("menu", views.menu, name="menu"),
-    path("menu/<str:name>", views.timing, name="timing"),
-    path("menu/<str:category_name>/<str:timing_name>", views.products, name="products"),
-    path("booking", views.book_table, name="book_table"), 
-    path("mybooks", views.my_books, name="mybooks"),  
-    path("contact", views.contact, name="contact"),  
-    path("menu/<str:category_name>/<str:timing_name>/<str:item_name>", views.viewitem, name="viewitem"),   
-    path('addtocart',views.add_to_cart,name="addtocart"),
-    path('cart',views.cart,name='cart'),
-    path('removecart/<str:id>',views.removecart,name='removecart'),
-    path('checkout',views.checkout,name='checkout'),
-    path('place-order',views.placeorder,name='placeorder'),
-    path('proceed-to-pay',views.razorpaycheck),
-    path('my-orders', views.myorders, name='my-orders'),
-    path('view-order/<str:trackno>',views.vieworder,name='orderview'),
-    path('download-pdf/<int:order_id>/', views.generate_pdf, name='download_pdf'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('pressure', views.pressure, name='pressure'),
     path('api/sensor-data/', views.get_sensor_data, name='get_sensor_data'),
@@ -37,4 +21,5 @@ urlpatterns = [
    
    
 ]
+
 
